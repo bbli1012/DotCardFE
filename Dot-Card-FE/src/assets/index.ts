df9baa5fbea4731b59902448162b5ea9b82a6587
Benjamin Li <61096974+bbli1012@users.noreply.github.com>
2023-08-01 22:53:22 -0700

    navbar

---
+++ Dot-Card-FE/src/assets/index.ts
@@ -0,0 +1,6 @@
+import logoDark from './logo-dark.svg';
+import logoLight from './logo-light.svg';
+import cart from './cart.svg';
+
+
+export { logoDark, logoLight, cart };
--- Dot-Card-FE/src/components/Navbar/Navbar.tsx
+++ Dot-Card-FE/src/components/Navbar/Navbar.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import "./Navbar.css"
+import { logoDark, cart } from "../../assets"
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="container">Navbar
+        <div className="navbar">
+            <img src={logoDark} alt="logo" />
+            <button className="cart-btn">
+                <img className="cart-btn-item" src={cart} alt="Cart" /> View Cart 
+            </button>
+        </div>
+    </div>
   )
 }
 
