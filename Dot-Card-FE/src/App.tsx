0cd0df887357f1af1965774f5807d9025f6aa825
Benjamin Li <61096974+bbli1012@users.noreply.github.com>
2023-08-01 22:19:50 -0700

    navbar and footer framework with routes

--- Dot-Card-FE/src/App.tsx
+++ Dot-Card-FE/src/App.tsx
@@ -1,7 +1,8 @@
 import './App.css'
-import Home from './pages/home/Home.tsx'
-import Cart from './pages/cart/Cart.tsx'
-import Product from './pages/product/Product.tsx'
+import Home from './pages/Home/Home.tsx'
+import Cart from './pages/Cart/Cart.tsx'
+import Product from './pages/Product/Product.tsx'
+import { Footer, Navbar } from './components'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
@@ -9,11 +10,13 @@ function App() {
   return (
     <>
       <Router>
+        <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/product/:productId' element={<Product />} />
           <Route path='/cart' element={<Cart />} />
         </Routes>
+        <Footer />
       </Router>
     </>
   )
---
+++ Dot-Card-FE/src/components/index.ts
@@ -0,0 +1,5 @@
+import Navbar from './Navbar/Navbar.tsx';
+import Footer from './Footer/Footer.tsx';
+
+
+export { Navbar, Footer };
