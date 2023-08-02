4e24b0d6389e595bf12ab92f73d5edd3e8b4f66a
Benjamin Li <61096974+bbli1012@users.noreply.github.com>
2023-08-01 22:57:10 -0700

    added link routing to navbar

--- Dot-Card-FE/src/components/Navbar/Navbar.tsx
+++ Dot-Card-FE/src/components/Navbar/Navbar.tsx
@@ -1,15 +1,21 @@
 import "./Navbar.css"
 import { logoDark, cart } from "../../assets"
+import { Link } from "react-router-dom";
+
 
 
 const Navbar = () => {
   return (
     <div className="container">Navbar
         <div className="navbar">
-            <img src={logoDark} alt="logo" />
-            <button className="cart-btn">
-                <img className="cart-btn-item" src={cart} alt="Cart" /> View Cart 
-            </button>
+            <Link to="/">
+                <img src={logoDark} alt="logo" />
+            </Link>
+            <Link to="/cart">
+                <button className="cart-btn">
+                    <img className="cart-btn-item" src={cart} alt="Cart" /> View Cart 
+                </button>
+            </Link>
         </div>
     </div>
   )
